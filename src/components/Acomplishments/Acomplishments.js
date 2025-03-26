83897d20d50afaf5c72c30ebc71bfbe4a7c565f6
Agam3108 <118785408+Agam3108@users.noreply.github.com>
2025-03-26 10:12:43 +0530

    init

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+
+const data = [
+  { number: 10, text: 'Dev Projects Created'},
+  { number: 600, text: 'DSA Problems Solved on Leetcode', },
+  { number: 1600, text: 'Max Rating on Leetcode', },
+  { number: 800, text: 'Connections on LinkedIn', }
+];
+
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider/>
+  </Section>
+);
+
+export default Acomplishments;
---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: 'College Management System',
+    description: "A cross-organisational platform for students, staff and head of departments for effective academic management.",
+      image: '/images/1.png',
+      tags: ['Postgres', 'Django', 'Javascript', 'Jquery'],
+    source: 'https://github.com/Agam3108/College_Management_System',
+    visit: 'https://mysite-8ffb.onrender.com/',
+    id: 0,
+  },
+  {
+    title: 'ExTrack',
+    description: "An expense tracker application through which users can manage their budgets and expenses and keep track of their monthly spending through dynamic bar-charts.",
+    image: '/images/2.png',
+    tags: ['Next.js', 'JavaScript', 'Drizzle-ORM','ClerkProvider'],
+    source: 'https://github.com/Agam3108/Expense_Tracker_1.1',
+    visit: 'https://expense-tracker-1-1.vercel.app/',
+    id: 1,
+  },
+  {
+    title: 'Stock Market Predictor',
+    description: "Using facebook prophet for making time-series forecasting & yahoo finance library I have created this app to predict the price of stock, four years from the current date. This app also displays the plot of opening and closing price of a stock using plotly. The UI is hosted on Streamlit. ",
+      image: '/images/3.png',
+      tags: ['Streamlit', 'Python'],
+    source: 'https://github.com/Agam3108/Stock_Prediction',
+    visit: 'https://stockprediction-cjq9un6dtuxkbiasmddhuh.streamlit.app/',
+    id: 2,
+  },
+  {
+    title: 'Fish Classfication & Recognition',
+    description: "Using Convolutional Neural Networks in Deep Learning, we have created this project to classify and recognize fish speices using images.",
+    image: '/images/4.png',
+    tags: ['Python', 'MobilenetV2', 'Jupyter'],
+    source: 'https://github.com/Agam3108/Minor_Project_Fish_Classification',
+    visit: 'https://minorprojectfishclassification-bv5qvw4yu3hpztabdg6r2h.streamlit.app/',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2022, text: 'Joined IIIT Bhopal, an Institute of National Importance', },
+  { year: 2023, text: 'Started my web-development journey', },
+  { year: 2023, text: 'Joined Google Developer Students Club as Content Writer', },
+ 
+];
